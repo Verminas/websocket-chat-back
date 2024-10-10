c32df8c2f870c0eaa0f756a6a2693f0e4a20ca75
Verminas <veremeeva96@gmail.com>
2024-10-10 16:42:55 +0300

    feat: add versel json for deploy

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import express, {Request, Response} from 'express'
+import {createServer} from 'http'
+import {Server} from "socket.io";
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3009;
+const initialMessagesData: Message[] = [{
+  id: 1,
+  name: 'Alex',
+  message: 'Hello, how are you7'
+}, {id: 2, name: 'Max', message: 'Do you watch a Harry Potter film?'}]
+
+const app = express();
+
+app.use(cors()); // Разрешить CORS для всех запросов
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    // allowedHeaders: ["my-custom-header"],
+    // credentials: true
+  }
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`hello on port ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('get-data', initialMessagesData)
+
+  socket.on('send-message', (data) => {
+    console.log(data);
+    const message = {message: data, name: 'Julia', id: Date.now()}
+    initialMessagesData.push(message);
+    io.emit('get-message', message)
+  })
+
+
+});
+
+
+server.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
+
+
+type Message = {
+  id: number
+  message: string
+  name: string
+}
